561ef0ca117ac252be667e91613d63611422f30d
thorzhou <thorzhou@hotmail.com>
2020-04-17 14:14:58 +0800

    初次创建工程，m3。

---
+++ Project/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Template_freertos' 
+ * Target:  'template' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FreeRTOS
+
+FreeRTOS v9.0 (V10是amazon2018收购后增加一些云组件)
