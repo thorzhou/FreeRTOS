39657948d2cd17907795e26d2ef684d81d2ccb1f
thorzhou <thorzhou@hotmail.com>
2020-04-18 15:14:27 +0800

    列表的实现

--- User/main.c
+++ User/main.c
@@ -1,7 +1,57 @@
+/**
+  ************************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2018-xx-xx
+  * @brief   ��FreeRTOS�ں�ʵ����Ӧ�ÿ���ʵսָ�ϡ��鼮����
+  *           �б����б���
+  ************************************************************************
+  * @attention
+  *
+  * ʵ��ƽ̨:Ұ�� STM32 ϵ�� ������
+  * 
+  * ����    :www.embedfire.com  
+  * ��̳    :http://www.firebbs.cn
+  * �Ա�    :https://fire-stm32.taobao.com
+  *
+  ************************************************************************
+  */
+  
+/*
+*************************************************************************
+*                             include
+*************************************************************************
+*/
+#include "list.h"
 
+struct xLIST List_Test;
 
+struct xLIST_ITEM List_Item1;
+struct xLIST_ITEM List_Item2;
+struct xLIST_ITEM List_Item3;
+
+/*
+************************************************************************
+*                                main����
+************************************************************************
+*/
 int main(void)
 {
+
+	vListInitalise(&List_Test);
+
+	vListInitialiseItem(&List_Item1);
+	List_Item1.xItemValue = 1;
+	vListInitialiseItem(&List_Item2);
+	List_Item2.xItemValue = 2;
+	vListInitialiseItem(&List_Item3);
+	List_Item3.xItemValue = 3;
+
+	vListInsert(&List_Test,&List_Item1);
+	vListInsert(&List_Test,&List_Item2);
+	vListInsert(&List_Test,&List_Item3);
+	
 	for(;;)
 	{}
 }
