8b929233fd0f33da2cdea42ae0ecee0559c38089
thorzhou <thorzhou@hotmail.com>
2020-04-19 20:20:07 +0800

    增加一些文件描述，临界段起个头。

--- DocumentDescription.py
+++ DocumentDescription.py
@@ -2,13 +2,17 @@
 import os,sys
 
 dic = {
-    r'.\User\main.c':'主程序入口',
-    r'.\freertos\list.c':'节点链表操作',
-    r'.\freertos\include\list.h':'列表操作声明文件',
-    r'.\freertos\include\portmacro.h':'不同平台下变量类型定义，将标准C类型重定义一个名字',
-    r'.\freertos\include\FreeRTOS.h':'平台相关数据类型包含文件',
-    r'.\freertos\include\FreeRTOSConfig.h':'系统时钟位数定义',
-    r'.\freertos\include\portable.h':'包含portmacro.h'
+    'main.c':'主程序入口',
+    'list.c':'节点链表操作',
+    'list.h':'列表操作声明文件',
+    'portmacro.h':'不同平台下变量类型定义，将标准C类型重定义一个名字，任务切换实现',
+    'FreeRTOS.h':'平台相关数据类型包含文件',
+    'FreeRTOSConfig.h':'系统相关定义，时钟，任务，优先级，中断函数声明等',
+    'portable.h':'包含portmacro.h',
+    'projdefs.h':'重定义函数指针，操作成功失败的声明',
+    'task.h':'创建任务的函数声明',
+    'task.c':'任务创建相关函数',
+    'port.c':'任务创建，加入就绪列表等操作函数'
 }
 
 def Query(filePath):
